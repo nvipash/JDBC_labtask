a8de4484963cf270552d42a779027ec23e59b821
Pavelchak <apavelchak@gmail.com>
2017-09-16 17:21:27 +0300

    Create new project JDBC Driver

---
+++ src/main/java/com/softserve/jdbctest/Aplication.java
@@ -0,0 +1,182 @@
+package com.softserve.jdbctest;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Aplication {
+    private static final String url = "jdbc:mysql://localhost:3306/db_jdbc";
+    private static final String user = "root";
+    private static final String password = "123456";
+
+    private static Connection connection=null;
+    private static Statement statement=null;
+    private static ResultSet rs=null;
+
+    public static void main(String args[]){
+        try {
+//region    0. This will load the MySQL driver, each DB has its own driver //
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //endregion
+
+
+//region    1. Get a connection to database //
+            connection = DriverManager.getConnection(url, user, password);
+            //endregion
+
+//region  2. Create a statement
+            // Statements allow to issue SQL queries to the database
+            statement=connection.createStatement();
+            //endregion
+
+            //readData();
+
+            updateDataCity();
+            //readData();
+
+            insertDataCity();
+
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver is not loaded");
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        } finally {
+            //close connection ,statement and resultset
+            if (rs != null) try { rs.close(); } catch (SQLException e) { } // ignore
+            if (statement != null) try { statement.close(); } catch (SQLException e) { }
+            if (connection != null) try { connection.close(); } catch (SQLException e) { }
+        }
+    }
+
+    private static void readData() {
+        try {
+//region    SELECT COUNT(*) FROM Person //
+            // 3. executing SELECT query
+            rs=statement.executeQuery("SELECT COUNT(*) FROM Person");
+
+            // 4. Process the result set
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                // Simply Print the results
+                System.out.format("count: %d\n", count);
+            }
+            //endregion
+
+//region    SELECT * FROM Person //
+            // 3. executing SELECT query
+            rs=statement.executeQuery("SELECT * FROM Person");
+
+            // 4. Process the result set
+            System.out.format("\nTable Person --------------------\n");
+            System.out.format("%3s %-12s %-12s %-10s %s\n", "ID", "Surname", "Name", "City", "Email");
+            while (rs.next())
+            {
+                int id=rs.getInt("IDPerson");
+                String surname = rs.getString("Surname");
+                String name = rs.getString("Name");
+                String city=rs.getString("City");
+                String email=rs.getString("Email");
+                // Simply Print the results
+                System.out.format("%3d %-12s %-12s %-10s %s\n", id, surname, name, city, email);
+            }
+            //endregion
+
+//region    SELECT * FROM Book //
+            // 3. executing SELECT query
+            rs=statement.executeQuery("SELECT * FROM Book");
+
+            // 4. Process the result set
+            System.out.format("\nTable Book --------------------\n");
+            System.out.format("%3s %-18s %-18s %s\n", "ID", "BookName", "Author", "Amount");
+            while (rs.next())
+            {
+                int id=rs.getInt("IDBook");
+                String bookName = rs.getString("BookName");
+                String author = rs.getString("Author");
+                String amount=rs.getString("Amount");
+                // Simply Print the results
+                System.out.format("%3d %-18s %-18s %s\n", id, bookName, author, amount);
+            }
+            //endregion
+
+//region    SELECT * FROM City //
+            // 3. executing SELECT query
+            rs=statement.executeQuery("SELECT * FROM City");
+
+            // 4. Process the result set
+            System.out.format("\nTable City --------------------\n");
+            System.out.format("%s\n", "City");
+            while (rs.next())
+            {
+                String city = rs.getString("City");
+                // Simply Print the results
+                System.out.format("%s\n", city);
+            }
+            //endregion
+
+//region    SELECT * FROM PersonBook //
+            // 3. executing SELECT query
+            String query="Select " +
+                    "(SELECT Surname FROM person WHERE IDPerson=P.IDPerson) AS Surname, " +
+                    "(SELECT BookName FROM book WHERE IDBook=P.IDBook) AS BookName "+
+                    "FROM personbook AS P";
+            rs=statement.executeQuery(query);
+
+            // 4. Process the result set
+            System.out.format("\nJoining Table PersonBook --------------------\n");
+            System.out.format("%-15s %s\n", "Surname", "BookName");
+            while (rs.next())
+            {
+                String surname = rs.getString("Surname");
+                String bookName = rs.getString("BookName");
+                // Simply Print the results
+                System.out.format("%-15s %s\n", surname, bookName);
+            }
+            //endregion
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    private static void updateDataCity(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input name city what you want to update: ");
+        String city = input.next();
+        System.out.println("Input new name city for %s: "+ city);
+        String citynew = input.next();
+
+        // 3. executing SELECT query
+        try {
+// 1
+            statement.execute("UPDATE city SET City='"+citynew+"' WHERE City='"+city+"';");
+
+// 2  Returns count of updated rows
+//            int n=statement.executeUpdate("UPDATE city SET City='"+citynew+"' WHERE City='"+city+"';");
+//            System.out.println("Count rows that updated: "+n);
+
+// 3  PreparedStatements can use variables and are more efficient
+//            PreparedStatement preparedStatement;
+//            preparedStatement=connection.prepareStatement("UPDATE city SET City=? WHERE City=?;");
+//            preparedStatement.setString(1, citynew);
+//            preparedStatement.setString(2, city);
+//            int n=preparedStatement.executeUpdate();
+//            System.out.println("Count rows that updated: "+n);
+
+        } catch (SQLException e) { e.printStackTrace(); }
+
+
+    }
+
+    private static void insertDataCity(){
+
+    }
+
+
+}
