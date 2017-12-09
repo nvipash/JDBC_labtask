56a508f0436a8e1f5d56016eea12160a0d2161fc
Pavel Novitskiy <novipsh@outlook.com>
2017-12-09 12:34:57 +0200

    Added my own database

--- src/main/resources/DataBase.sql
+++ src/main/resources/DataBase.sql
@@ -1,104 +1,73 @@
-CREATE SCHEMA IF NOT EXISTS `DB_JDBC` DEFAULT CHARACTER SET utf8;
-USE `DB_JDBC`;
+create database if not exists lab_5a;
+use lab_5a;
 
-CREATE TABLE City 
+create table if not exists train (
+    id_train 			int auto_increment primary key,
+    number_train 		int not null,
+    type_train 			varchar(25) not null,
+    photo_train		    varchar(25) not null
+);
+
+insert into train (number_train, type_train, photo_train) values 
+				  (122,'high-speed','photo-1'),
+				  (891,'low-speed','photo-2'),
+			      (98,'electric-regional','photo-3'),
+				  (142,'low-speed','photo-4');
+
+create table if not exists coach
 (
-  City VARCHAR(25) NOT NULL,
-  PRIMARY KEY (City)
-) ENGINE = InnoDB;
+	id_coach              int auto_increment primary key,
+	number_place		  int not null,
+	type_coach 			  varchar(25) not null,
 
-CREATE TABLE  Person 
+	constraint coach_train_id_fk
+		foreign key (id_coach) references train (id_train)
+);
+
+alter table coach
+	add constraint CK_number_place
+	check (number_place > 1 AND number_place < 56);
+
+insert into coach (number_place, type_coach) values 
+				  (32,'kupe'),
+			      (54,'plackart'),
+				  (12,'lux'),
+				  (3,'plackart');
+
+create table if not exists ticket_coach (
+	id_ticket_coach int auto_increment primary key,
+    coach_id int null,
+    number_coach int not null,
+    
+    constraint ticketcoach_coach_id_fk 
+		foreign key (coach_id) references coach (id_coach)
+);
+
+insert into ticket_coach (number_coach) values 
+						 (12), (7), (9), (2);	
+
+create table if not exists ticket
 (
-  IDPerson INT NOT NULL AUTO_INCREMENT,
-  Surname VARCHAR(25) NOT NULL,
-  `Name` VARCHAR(25) NOT NULL,
-  City VARCHAR(25) NOT NULL,  
-  Email VARCHAR(45) NULL,
-  PRIMARY KEY (IDPerson),
-  CONSTRAINT FOREIGN KEY (City) 
-    REFERENCES City (City)
-) ENGINE = InnoDB;
-
-CREATE TABLE Book 
-(
-  IDBook INT NOT NULL AUTO_INCREMENT,
-  BookName VARCHAR(45) NOT NULL,
-  Author VARCHAR(45) NOT NULL,
-  Amount INT NOT NULL,
-  PRIMARY KEY (IDBook)
-) ENGINE = InnoDB;
-
-CREATE TABLE  PersonBook (
-  IDPerson INT NOT NULL,
-  IDBook INT NOT NULL,
-  PRIMARY KEY (IDPerson, IDBook),
-  CONSTRAINT  FOREIGN KEY (IDPerson)
-    REFERENCES  Person (IDPerson),
-  CONSTRAINT   FOREIGN KEY (IDBook)
-    REFERENCES  Book (IDBook)
-) ENGINE = InnoDB;
-
-INSERT INTO `book` VALUES 
-(1,'Bible','St. mans',5),
-(2,'Kobzar','Shevchenko ',4),
-(3,'Harry Potter','J. K. Rowling',1),
-(4,'Zakhar Berkut','I. Franko',2),
-(5,'The Jungle Book','Rudyard Kipling',1);
-
-INSERT INTO `city` VALUES ('Herson'),('Kyiv'),('Lviv'),('Poltava'),('Ternopil');
-
-INSERT INTO `person` VALUES 
-(1,'Koldovskyy','Vyacheslav','Lviv','koldovsky@gmail.com'),
-(2,'Pavelchak','Andrii','Poltava','apavelchak@gmail.com'),
-(3,'Soluk','Andrian','Herson','andriansoluk@gmail.com'),
-(4,'Dubyniak','Bohdan','Ternopil','bohdan.dub@gmail.com'),
-(5,'Faryna','Igor','Kyiv','farynaihor@gmail.com'),
-(6,'Kurylo','Volodymyr','Poltava','kurylo.volodymyr@gmail.com'),
-(7,'Matskiv','Marian','Herson','marian3912788@gmail.com'),
-(8,'Shyika','Tamara','Kyiv','tamara.shyika@gmail.com'),
-(9,'Tkachyk','Volodymyr','Ternopil','vova1234.tkachik@gmail.com');
-
-INSERT INTO `personbook` VALUES (4,1),(5,1),(8,1),(2,2),(6,2),(7,2),(1,3),(1,4),(9,4),(3,5);
-
-DELIMITER //
-CREATE PROCEDURE InsertPersonBook
-(
-IN SurmanePersonIn varchar(25),
-IN BookNameIN varchar(45)
-)
-BEGIN
-	DECLARE msg varchar(40);
+	id_ticket             int auto_increment primary key,
+	date_departuere       date not null,
+	place_departuere	  varchar(25) not null,
+	tram_schedule         text not null,
+	place_arrival		  varchar(25) not null,
+	price_ticket		  int not null,
+	status_ticket         varchar(25) not null,
     
-  -- checks for present Surname
-  IF NOT EXISTS( SELECT * FROM Person WHERE Surname=SurmanePersonIn)
-  THEN SET msg = 'This Surname is absent';
-    
-  -- checks for present Book
-	ELSEIF NOT EXISTS( SELECT * FROM Book WHERE BookName=BookNameIN)
-		THEN SET msg = 'This Book is absent';
-    
-  -- checks if there are this combination already
-	ELSEIF EXISTS( SELECT * FROM personbook 
-		WHERE IDPerson = (SELECT IDPerson FROM Person WHERE Surname=SurmanePersonIn)
-        AND IDBook = (SELECT IDBook FROM Book WHERE BookName=BookNameIN)
-        )
-        THEN SET msg = 'This Person already has this book';
-	
-  -- checks whether there is still such a book
-	ELSEIF (SELECT Amount FROM Book WHERE BookName=BookNameIN ) 
-    <= (SELECT COUNT(*) FROM personbook WHERE IDBook=(SELECT IDBook FROM Book WHERE BookName=BookNameIN) )
-    THEN SET msg = 'There are no this Book already';
-    
-    -- makes insert
-    ELSE 
-		INSERT personbook (IDPerson, IDBook) 
-        Value ( (SELECT IDPerson FROM Person WHERE Surname=SurmanePersonIn),
-			     (SELECT IDBook FROM Book WHERE BookName=BookNameIN) );
-		SET msg = 'OK';		 
+    constraint ticket_ticketcoach_id_fk 
+		foreign key (id_ticket) references ticket_coach (id_ticket_coach)
+);
 
-	END IF;
+alter table ticket
+	add constraint CK_price_ticket
+	check (price_ticket >= 5),
+	add constraint CK_status_ticket
+	check (status_ticket in ('free', 'sold', 'booked'));
 
-	SELECT msg AS msg;
-
-END //
-DELIMITER ;
+insert into ticket (date_departuere, place_departuere, tram_schedule, place_arrival, price_ticket, status_ticket) values 
+				   ('2017-10-09', 'Lviv','shedule-1', 'Kyiv', 15, 'booked'),
+				   ('2018-06-29', 'Dnipro','shedule-2', 'Chernivtsi', 28, 'sold'),
+				   ('2017-02-05', 'Zaporizha','shedule-2', 'Uzhorod', 32, 'free'),
+				   ('2017-04-22', 'Rivne','shedule-2', 'Kropyvnitskyi', 32, 'free');
