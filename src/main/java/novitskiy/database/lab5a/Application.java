c345285d2ac8462984af5d8484d1d6a4914fde1a
Pavel Novitskiy <novipsh@outlook.com>
2017-12-10 01:57:51 +0200

    Works with some tables
    works with some tables: ticket, train, coach; not work with ticket_coach

--- src/main/java/novitskiy/database/lab5a/Application.java
+++ src/main/java/novitskiy/database/lab5a/Application.java
@@ -4,41 +4,21 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Application {
-    private static final String url = "jdbc:mysql://localhost:3306/db_jdbc?serverTimezone=UTC&useSSL=false";
+    private static final String url = "jdbc:mysql://localhost:3306/lab_5a?serverTimezone=UTC&useSSL=false";
     private static final String user = "root";
     private static final String password = "0000";
 
     private static Connection connection = null;
     private static Statement statement = null;
-    private static ResultSet rs = null;
+    private static ResultSet resultSet = null;
 
     public static void main(String args[]) {
         try {
-//region    0. This will load the MySQL driver, each DB has its own driver //
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            //endregion
-
-
-//region    1. Get a connection to database //
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
-            //endregion
-
-//region  2. Create a statement
-            // Statements allow to issue SQL queries to the database
             statement = connection.createStatement();
-            //endregion
 
             readData();
-
-//            updateDataCity();
-//            readData();
-
-//            insertDataCity();
-
-//            DeleteDataCity();
-
-//            CallProcedureForInsertToPersonBook();
-
 
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL Driver is not loaded");
@@ -50,8 +30,8 @@ public class Application {
 
         } finally {
             //close connection ,statement and resultset
-            if (rs != null) try {
-                rs.close();
+            if (resultSet != null) try {
+                resultSet.close();
             } catch (SQLException e) {
             } // ignore
             if (statement != null) try {
@@ -66,153 +46,162 @@ public class Application {
     }
 
     private static void readData() throws SQLException {
-//region    SELECT COUNT(*) FROM Person //
+//region    SELECT COUNT(*) FROM train //
         // 3. executing SELECT query
-        rs = statement.executeQuery("SELECT COUNT(*) FROM Person");
+        resultSet = statement.executeQuery("SELECT COUNT(*) FROM train");
 
         // 4. Process the result set
-        while (rs.next()) {
-            int count = rs.getInt(1);
+        while (resultSet.next()) {
+            int count = resultSet.getInt(1);
             // Simply Print the results
             System.out.format("\ncount: %d\n", count);
         }
         //endregion
 
-//region    SELECT * FROM Person //
+//region    SELECT * FROM train //
         // 3. executing SELECT query
-        rs = statement.executeQuery("SELECT * FROM Person");
+        resultSet = statement.executeQuery("SELECT * FROM train");
 
         // 4. Process the result set
-        System.out.format("\nTable Person --------------------\n");
-        System.out.format("%3s %-12s %-12s %-10s %s\n", "ID", "Surname", "Name", "City", "Email");
-        while (rs.next()) {
-            int id = rs.getInt("IDPerson");
-            String surname = rs.getString("Surname");
-            String name = rs.getString("Name");
-            String city = rs.getString("City");
-            String email = rs.getString("Email");
+        System.out.format("\nTable Train ------------------------------------------------------------\n");
+        System.out.format("%-18s %-18s %-18s %s\n", "id_train", "number_train", "type_train", "photo_train");
+        while (resultSet.next()) {
+            int idTrain = resultSet.getInt("id_train");
+            int numberTrain = resultSet.getInt("number_train");
+            String typeTrain = resultSet.getString("type_train");
+            String photoTrain = resultSet.getString("photo_train");
             // Simply Print the results
-            System.out.format("%3d %-12s %-12s %-10s %s\n", id, surname, name, city, email);
+            System.out.format("%-18s %-18s %-18s %s\n", idTrain, numberTrain, typeTrain, photoTrain);
         }
         //endregion
 
-//region    SELECT * FROM Book //
+//region    SELECT * FROM coach //
         // 3. executing SELECT query
-        rs = statement.executeQuery("SELECT * FROM Book");
+        resultSet = statement.executeQuery("SELECT * FROM coach");
 
         // 4. Process the result set
-        System.out.format("\nTable Book --------------------\n");
-        System.out.format("%3s %-18s %-18s %s\n", "ID", "BookName", "Author", "Amount");
-        while (rs.next()) {
-            int id = rs.getInt("IDBook");
-            String bookName = rs.getString("BookName");
-            String author = rs.getString("Author");
-            String amount = rs.getString("Amount");
+        System.out.format("\nTable Coach ------------------------------------------------------------\n");
+        System.out.format("%-18s %-18s %s\n", "id_coach", "number_place", "type_coach");
+        while (resultSet.next()) {
+            int idCoach = resultSet.getInt("id_coach");
+            int numberPlace = resultSet.getInt("number_place");
+            String typeCoach = resultSet.getString("type_coach");
             // Simply Print the results
-            System.out.format("%3d %-18s %-18s %s\n", id, bookName, author, amount);
+            System.out.format("%-18s %-18s %s\n", idCoach, numberPlace, typeCoach);
         }
         //endregion
 
-//region    SELECT * FROM City //
+//region    SELECT * FROM ticket //
         // 3. executing SELECT query
-        rs = statement.executeQuery("SELECT * FROM City");
+        resultSet = statement.executeQuery("SELECT * FROM ticket");
 
         // 4. Process the result set
-        System.out.format("\nTable City --------------------\n");
-        System.out.format("%s\n", "City");
-        while (rs.next()) {
-            String city = rs.getString("City");
+        System.out.format("\nTable Ticket ------------------------------------------------------------------------------------------------------\n");
+        System.out.format("%-12s %-18s %-18s %-15s %-18s %-15s %s\n", "id_ticket", "date_departuere", "place_departuere", "tram_schedule", "place_arrival", "price_ticket", "status_ticket");
+        while (resultSet.next()) {
+            int idTicket = resultSet.getInt("id_ticket");
+            String dateDepartuere = resultSet.getString("date_departuere");
+            String placeDepartuere = resultSet.getString("place_departuere");
+            String tramSchedule = resultSet.getString("tram_schedule");
+            String placeArrival = resultSet.getString("place_arrival");
+            int priceTicket = resultSet.getInt("price_ticket");
+            String statusTicket = resultSet.getString("status_ticket");
             // Simply Print the results
-            System.out.format("%s\n", city);
+            System.out.format("%-12s %-18s %-18s %-15s %-18s %-15s %s\n", idTicket, dateDepartuere, placeDepartuere, tramSchedule, placeArrival, priceTicket, statusTicket);
         }
         //endregion
 
-//region    SELECT * FROM PersonBook //
+//region    SELECT * FROM ticket_coach //
         // 3. executing SELECT query
-        String query = "Select " +
-                "(SELECT Surname FROM person WHERE IDPerson=P.IDPerson) AS Surname, " +
-                "(SELECT BookName FROM book WHERE IDBook=P.IDBook) AS BookName " +
-                "FROM personbook AS P";
-        rs = statement.executeQuery(query);
+        String queryTicketCoach = "SELECT (SELECT status_ticket FROM ticket WHERE id_ticket = ticket_coach.id_ticket_coach) AS ticket, " +
+                "(SELECT number_place FROM coach WHERE id_coach = ticket_coach.coach_id) AS coach, number_coach FROM ticket_coach";
+        resultSet = statement.executeQuery(queryTicketCoach);
 
         // 4. Process the result set
-        System.out.format("\nJoining Table PersonBook --------------------\n");
-        System.out.format("%-15s %s\n", "Surname", "BookName");
-        while (rs.next()) {
-            String surname = rs.getString("Surname");
-            String bookName = rs.getString("BookName");
+        System.out.format("\nJoining Table TicketCoach ------------------------------------------------------------\n");
+        System.out.format("%-18s %-15s %s\n", "id_ticket_coach", "coach_id", "number_coach");
+        while (resultSet.next()) {
+            int idTicketCoach = resultSet.getInt("id_ticket_coach");
+            int coachId = resultSet.getInt("coach_id");
+            int numberCoach = resultSet.getInt("number_coach");
             // Simply Print the results
-            System.out.format("%-15s %s\n", surname, bookName);
+            System.out.format("%-18s %-15s %s\n", idTicketCoach, coachId, numberCoach);
         }
         //endregion
 
     }
 
-    private static void updateDataCity() throws SQLException {
+    private static void updateDataStatusTicket() throws SQLException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input name city what you want to update: ");
-        String city = input.next();
-        System.out.println("Input new name city for %s: " + city);
-        String citynew = input.next();
+        System.out.println("Input status of ticket what you want to update: ");
+        String statusTicketOld = input.next();
+        System.out.println("Input new status of ticket for %s: " + statusTicketOld);
+        String statusTicketNew = input.next();
 
-        // 3. executing SELECT query
-// 1
-        statement.execute("UPDATE city SET City='" + citynew + "' WHERE City='" + city + "';");
-
-// 2  Returns count of updated rows
-//        int n=statement.executeUpdate("UPDATE city SET City='"+citynew+"' WHERE City='"+city+"';");
-//        System.out.println("Count rows that updated: "+n);
-
-// 3  PreparedStatements can use variables and are more efficient
-//        PreparedStatement preparedStatement;
-//        preparedStatement=connection.prepareStatement("UPDATE city SET City=? WHERE City=?;");
-//        preparedStatement.setString(1, citynew);
-//        preparedStatement.setString(2, city);
-//        int n=preparedStatement.executeUpdate();
-//        System.out.println("Count rows that updated: "+n);
-
+        statement.execute("UPDATE ticket SET status_ticket=" + statusTicketNew + "' WHERE status_ticket='" + statusTicketOld + "';");
     }
 
-    private static void insertDataCity() throws SQLException {
+    private static void insertDataPlaceDepartuere() throws SQLException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input a new name city: ");
-        String newcity = input.next();
+        System.out.println("Input a new place departuere: ");
+        String newPlaceDepartuere = input.next();
 
-        // 3. executing SELECT query
-        //   PreparedStatements can use variables and are more efficient
         PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement("INSERT city VALUES (?)");
-        preparedStatement.setString(1, newcity);
+        preparedStatement = connection.prepareStatement("INSERT ticket VALUES (?)");
+        preparedStatement.setString(1, newPlaceDepartuere);
         int n = preparedStatement.executeUpdate();
         System.out.println("Count rows that inserted: " + n);
 
     }
 
-    private static void DeleteDataCity() throws SQLException {
+    private static void insertDataPlaceArrival() throws SQLException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input a name city for delete: ");
-        String city = input.next();
+        System.out.println("Input a new place arrival: ");
+        String newPlaceArrival = input.next();
 
-        // 3. executing SELECT query
-        //   PreparedStatements can use variables and are more efficient
         PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement("DELETE FROM city WHERE City=?");
-        preparedStatement.setString(1, city);
+        preparedStatement = connection.prepareStatement("INSERT ticket VALUES (?)");
+        preparedStatement.setString(1, newPlaceArrival);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that inserted: " + n);
+
+    }
+
+    private static void deleteDataPlaceDepartuere() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a departuere place for delete: ");
+        String placeDepartuere = input.next();
+
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("DELETE FROM ticket WHERE place_departuere=?");
+        preparedStatement.setString(1, placeDepartuere);
         int n = preparedStatement.executeUpdate();
         System.out.println("Count rows that deleted: " + n);
     }
 
-    private static void CallProcedureForInsertToPersonBook() throws SQLException {
+    private static void deleteDataPlaceArrival() throws SQLException {
         Scanner input = new Scanner(System.in);
-        System.out.println("\nInput Surname for Person: ");
-        String surname = input.next();
-        System.out.println("Input NameBook for Book: ");
-        String book = input.next();
+        System.out.println("Input a arrival place for delete: ");
+        String placeArrival = input.next();
+
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("DELETE FROM ticket WHERE place_arrival=?");
+        preparedStatement.setString(1, placeArrival);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that deleted: " + n);
+    }
+
+    private static void CallProcedureForInsertToTicketCoach() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nInput status of ticket for ticket: ");
+        String statusTicket = input.next();
+        System.out.println("Input number of place for coach: ");
+        String numberPlace = input.next();
 
         CallableStatement callableStatement;
-        callableStatement = connection.prepareCall("{call InsertPersonBook(?, ?)}");
-        callableStatement.setString("SurmanePersonIn", surname);
-        callableStatement.setString("BookNameIN", book);
+        callableStatement = connection.prepareCall("{call InsertStatusTicket(?, ?)}");
+        callableStatement.setString("StatusTicketIn", statusTicket);
+        callableStatement.setString("NumberPlaceCoachIN", numberPlace);
         ResultSet rs = callableStatement.executeQuery();
 
         while (rs.next()) {
